24cc1be7745c42dfe578feb5b1cc087e388f548a
cwmanning <chris.manning@viget.com>
2014-02-13 22:14:09 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>HTML5 video demo</title>
+	</head>
+	<body>
+		<video id="video" autoplay preload>
+			<source src="video/pitcher.mp4" type="video/mp4">
+		</video>
+	</body>
+</html>
